99e846d88ac321da04056b5d60d6896448bb7cf4
kunkl007 <kunkl007@umn.edu>
2019-10-16 10:53:13 -0500

    its just the header dude

---
+++ csci4061/Template/src/phase1.h
@@ -0,0 +1,11 @@
+#ifndef PHASE1_H
+#define PHASE1_H
+
+void phase1(char* filepath, int m, char mapArray[][28]);
+
+
+#endif /*PHASE1_H*/
+// Add Guard to header file (google what are guards)
+// Function prototypes to 
+//			Traverse the Folder
+//			Partition the text file paths to 'm' files 
